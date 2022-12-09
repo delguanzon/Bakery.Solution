a87ca13ff1eea107373d8280b56d414b0379df64
delguanzon <yodelguanzon@gmail.com>
2022-12-09 11:28:08 -0800

    Add plain english specs for testing, create first test for bread class

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreateAndInstanceOfBread_Bread()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Bread bread = new Bread();
+      Assert.AreEqual(typeof(Bread), bread.GetType());
+    }
+
+  }
+}
--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,6 +1,6 @@
-namespace ProjectName
+namespace Bakery.Models
 {
-  public class ClassName
+  public class Bread
   {
     // properties, constructors, methods, etc. go here
   }
